ff2b265a7ddf28bf13345042531abbf940e816ec
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2018-11-10 20:26:09 +0100

    set spotify access token after login and display username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { async } from '@angular/core/testing';
+import { spotify } from './config/spotify-api.config';
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { spotifyAuthConfig } from './config/spotify-auth.config';
@@ -9,16 +11,21 @@ import { spotifyAuthConfig } from './config/spotify-auth.config';
 })
 export class AppComponent {
   title = 'wikidata-spotify-matcher';
+  username = '';
 
   constructor(private oauthService: OAuthService) {
-    this.configureSpotifyAuth();
+    this.configureSpotifyAuth()
+      .then(() => spotify.getMe())
+      .then(me => this.username = me.display_name);
   }
 
   private configureSpotifyAuth() {
     this.oauthService.configure(spotifyAuthConfig);
     this.oauthService.setStorage(sessionStorage);
-    this.oauthService.tryLogin().then((isSuccess) => {
-      if (!isSuccess) {
+    return this.oauthService.tryLogin().then((isSuccess) => {
+      if (isSuccess) {
+        spotify.setAccessToken(this.oauthService.getAccessToken());
+      } else {
         this.oauthService.initImplicitFlow();
       }
     });
