caec51bedba393554789780a91561973ce945036
Norbert Somogyi <norbertss11@gmail.com>
2018-11-24 15:07:00 +0100

    Show artist names and track title

--- src/app/components/library-tree/library-tree.component.ts
+++ src/app/components/library-tree/library-tree.component.ts
@@ -24,6 +24,7 @@ export class LibraryTreeComponent implements OnInit, AfterViewChecked {
 
   matchingWikidataSongs: WdkSongWrapper[] = [];
   trackSelected = false;
+  matchFound = true;
 
   private unfilteredLibrary: ArtistWithAlbums[];
   private viewCheckedCallback: any;
@@ -131,7 +132,7 @@ export class LibraryTreeComponent implements OnInit, AfterViewChecked {
   async onTrackSelected(track: SpotifyApi.TrackObjectFull) {
     this.matchingWikidataSongs = [];
     this.trackSelected = true;
-    
+
     const entity = await this.wdk.getSongBySpotifyId(track.id);
     if (entity) {
       console.log('Yeah we found a song in wikidata by its spotify id!');
@@ -141,6 +142,7 @@ export class LibraryTreeComponent implements OnInit, AfterViewChecked {
     const entities = await this.wdk.findSongsByTitle(track.name);
     if (!entities || entities.length === 0) {
       console.warn('Not found in wikidata: ' + track.name);
+      this.matchFound = false;
       return;
     }
     console.log('Found songs in wikidata for: ' + track.name);
@@ -151,12 +153,17 @@ export class LibraryTreeComponent implements OnInit, AfterViewChecked {
       const artists = song.artists.map(a => a.labels.en).join(', ');
       console.log(`${artists}: ${song.name} (${albums}) - ${song.description}`);
 
+      this.matchFound = true;
       this.matchingWikidataSongs.push(song);
       console.log('------------------------');
       console.log(song);
       console.log(albums);
       console.log(artists);
     });
+  }
+
+  private getArtists(song: WdkSongWrapper) {
+    return song.artists.map(a => a.labels['en']).join(', ');
   }
 
 }
